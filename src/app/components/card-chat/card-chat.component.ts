e239b60a20f64e4a98bca4acfa07a300698299bf
Eric Almeida <ericalmeida.sp@gmail.com>
2023-09-05 19:19:08 -0300

    implementação em Angular

---
+++ src/app/components/card-chat/card-chat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-chat',
+  templateUrl: './card-chat.component.html',
+  styleUrls: ['./card-chat.component.css']
+})
+export class CardChatComponent {
+
+}
---
+++ src/app/models/quadro.model.spec.ts
@@ -0,0 +1,7 @@
+import { Quadro } from './quadro.model';
+
+describe('Quadro', () => {
+  it('should create an instance', () => {
+    expect(new Quadro()).toBeTruthy();
+  });
+});
